7a247a8c709ba7edeec9b7f8f5d1ca4079b6ee0c
seacat271 <seacat271083@gmail.com>
2022-08-13 15:28:07 +0300

    done task#1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,43 @@
+const refs = {
+    startBtn: document.querySelector("[data-start]"),
+    stopBtn: document.querySelector("[data-stop]"),
+    body: document.querySelector("body")
+}
 
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function changeColor() { 
+    randomColor =  getRandomHexColor() 
+    refs.body.style.backgroundColor = randomColor
+}
+
+function BtnDisabler () {
+    let arr = document.querySelectorAll("button");
+    [...arr].map(item => item.disabled = !item.disabled)
+}
+
+const TIME_DELAY = 1000;
+let colorTimerId = null;
+refs.stopBtn.disabled = true; 
+
+
+function onClickStartBtn() {
+    changeColor()
+    colorTimerId = setInterval(() => {
+        changeColor()}, TIME_DELAY);
+    BtnDisabler () 
+}
+
+function onClickStopBtn() {
+    clearInterval(colorTimerId);
+    BtnDisabler ()   
+}
+
+
+
+
+refs.startBtn.addEventListener("click", onClickStartBtn)
+
+refs.stopBtn.addEventListener("click", onClickStopBtn)
