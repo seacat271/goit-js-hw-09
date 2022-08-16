28851bc9894979b5a6f11a8128c76e40255c2093
seacat271 <seacat271083@gmail.com>
2022-08-16 10:39:18 +0300

    task #3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,31 @@
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  const promise = new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+  setTimeout(() => {
+    if (shouldResolve) {
+      resolve({position, delay})
+      } else {
+      reject({ position, delay })
+      }
+  }, 1000)
+  })}
+
+
+const refs = {
+  inputForm: document.querySelector(".form"),
 }
+
+refs.inputForm.addEventListener("submit", (event) =>{
+  event.preventDefault()
+  createPromise()
+})
+
+
+
+createPromise(2, 1500)
+  .then(({position, delay}) => {
+    console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+  })
+  .catch(({ position, delay }) => {
+    console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+  });
