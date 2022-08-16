68b51012d94e0ebc68649a776d1d6c68766ac7ed
seacat271 <seacat271083@gmail.com>
2022-08-16 16:12:51 +0300

    Done task#3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,13 +1,4 @@
-import { Report } from 'notiflix/build/notiflix-report-aio';
-
-function createPromise(position, delay) { 
-   return new Promise((resolve, reject) => {
-    const shouldResolve = Math.random() > 0.3;
-  setTimeout(() => {
-    if (shouldResolve) resolve({ position, delay });
-    reject({ position, delay });
-    }, delay)
-  })}
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 const refs = {
   inputForm: document.querySelector(".form"),
@@ -15,22 +6,31 @@ const refs = {
 
 refs.inputForm.addEventListener("submit", (event) =>{
   event.preventDefault()
-  let { elements: {delay: startDelay, step, amount} } = event.target;
-  console.log(startDelay.value, step.value, amount.value)
+  let { delay: startDelay, step, amount } = event.target.elements;
 
-for (let i = 1; i <= +amount.value; i += 1){
-    delay = +startDelay.value + step.value * (i - 1);
-  createPromise(i, delay)
-  .then(({ position, delay }) => {
-    console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
-  })
-  .catch(({ position, delay }) => {
-    console.log(`❌ Rejected promise ${position} in ${delay}ms`);
-  })
- 
-}
+  generatePromises(startDelay, step, amount);
+  resetInputForm(startDelay, step, amount);
+
 })
 
+function generatePromises (a, b, c) {
+  for (let i = 1; i <= c.value; i += 1){
+    delay = +a.value + b.value * (i - 1);
+  createPromise(i, delay)
+  .then(({ position, delay }) => {Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`)})
+  .catch(({ position, delay }) => {Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`)})
+}}
+
+function createPromise(position, delay) { 
+    return new Promise((resolve, reject) => {
+     const shouldResolve = Math.random() > 0.3;
+   setTimeout(() => {
+     if (shouldResolve) resolve({ position, delay });
+     reject({ position, delay });
+     }, delay)
+   })}
 
 
-
+function resetInputForm(startDelay, step, amount) {
+  [...arguments].forEach(elem => elem.value = "")
+}
